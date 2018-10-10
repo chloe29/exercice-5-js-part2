f553ef200eefc1fcb22dd56a9508db7447f0a250
lavisse <chloe.lavisse80@gmail.com>
2018-10-10 14:05:27 +0200

    envoi des fichiers

---
+++ script.js
@@ -0,0 +1,13 @@
+function change(){
+  document.getElementById('image1').src="assets/image1_2.jpg";
+  document.getElementById('image2').src="assets/image2_2.jpg";
+  document.getElementById('image3').src="assets/image3_2.jpg";
+  document.getElementById('image4').src="assets/image4_2.jpg";
+  document.getElementById('image5').src="assets/image5_2.jpg";
+}
+// pour changer les images une par une :
+/*
+function change(image){
+  image.src = 'assets/' + image.id + '_2.jpg';
+}
+*/
